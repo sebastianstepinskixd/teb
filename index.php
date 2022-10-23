22000c784b6d63e381e9bb6e5ec9853d7ce8f6f0
sebastianstepinskixd <sebastianstepinski.1@gmail.com>
2022-10-23 13:57:43 +0200

    wylaczenie wyswietlania bledow dla wersji produkcyjnej, zmiana parametru w funkcji render

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+//wersja produkcyjna
+//error_reporting(0);
+//init_set('display_errors', '0');
+
 require_once('./src/view.php');
 include_once('./src/utils/debug.php');
 
@@ -12,11 +16,17 @@ $action = $_GET['action'] ?? DEFAULT_ACTION;
 $params = [];
 
 if($action == 'create')
+{
+	$page = 'create';
 	$params['resultCreate'] = "Udało się dodać notatkę";
+}
 else
+{
+	$page = 'list';
 	$params['resultList'] = "Wyświetlono listę notatek";
+}
 
 $view = new View();
-$view->render($action, $params);
+$view->render($page, $params);
 
 ?>
