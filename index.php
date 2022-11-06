32469997de26d48f88ec56aff2fdcdbea8556ec2
sebastianstepinskixd <sebastianstepinski.1@gmail.com>
2022-11-06 13:44:14 +0100

    Obsuga wyjatkow - polaczenie z baza

--- index.php
+++ index.php
@@ -11,9 +11,29 @@ require_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 require_once('./config/config.php');
 
-Controller::initConfiguration($configuration);
 
-$c = new Controller();
-$c -> run();
+require_once('./Exception/AppException.php');
+require_once('./Exception/StorageException.php');
+require_once('./Exception/ConfigurationException.php');
+
+use App\Exception\AppException;
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use Throwable;
+
+try{
+    Controller::initConfiguration($configuration);
+    $c = new Controller();
+    $c -> run();
+
+} catch (AppException $e)
+{
+    echo "<h1>Wystapił błąd w aplikacji</h1>";
+    echo '<h3>' . $e -> getMessage() . '</h3>';
+} catch (Throwable $e)
+{
+    echo "<h1>Wystapił błąd w aplikacji</h1>";
+}
+
 
 ?>
--- src/Database.php
+++ src/Database.php
@@ -5,12 +5,23 @@ declare(strict_type=1);
 namespace App;
 
 use PDO;
-
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use PDOException;
+use Throwable;
 class Database
 {
     public function __construct($config)
     {
         $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
         $connection = new PDO($dsn);
+    }
+
+    private function validateConfig($config)
+    {
+        if(empty($config['database']) || empty($config['user']) || empty($config['host']))
+        {
+            throw new ConfigurationException("Porblem z konfiguracją - skontaktuj się z administratorem.");
+        }
     }
 }
